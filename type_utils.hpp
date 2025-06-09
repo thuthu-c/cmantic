77213ecbad4c4f6136eddda105634575b17ad846
ronaque <isaque.bm9@gmail.com>
2025-06-09 10:03:35 -0300

    docs

--- type_utils.hpp
+++ type_utils.hpp
@@ -26,6 +26,13 @@ enum class PrimitiveType {
     UNDEFINED
 };
 
+/**
+ * Represents a variable type in the symbol table.
+ * It can be a primitive type, a reference to another type, or a record type.
+ * The `p_type` field indicates the primitive type.
+ * The `record_name` field is used for record types.
+ * The `referenced_type` field is used for reference types.
+ */
 struct VarType {
     PrimitiveType p_type;
     std::optional<std::string> record_name;
@@ -100,6 +107,10 @@ struct VarType {
     }
 };
 
+/**
+ * Represents a field in a record or a parameter in a procedure.
+ * It contains a name and a type.
+ */
 struct ParamField {
     std::string name;
     VarType type;
@@ -120,6 +131,13 @@ struct Record {
 
 using SymbolContent = std::variant<Procedure, Record, Variable>;
 
+/**
+ * Represents a symbol in the symbol table.
+ * It can be a procedure, record, or variable.
+ * The `name` field is the identifier of the symbol.
+ * The `category` field indicates the type of symbol. (e.g., PROCEDURE, RECORD, VARIABLE).
+ * The `content` field holds the specific details of the symbol. (e.g., procedure details, record fields, or variable type).
+ */
 struct Symbol {
     std::string name;
     SymbolCategory category;
